187937988d990697b776ffe510ea1428431ff74f
thpeet <thpeet@fastmail.org>
2021-11-17 10:15:13 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/jokapplication/JokApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokApplication.class, args);
+	}
+
+}
