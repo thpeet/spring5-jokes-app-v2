07261481115068d6b586a946e70d3f989dc714ee
thpeet <thpeet@fastmail.org>
2021-11-17 11:05:10 +0100

    Implement controller and template

--- src/main/java/guru/springframework/jokapplication/controllers/JokesController.java
+++ src/main/java/guru/springframework/jokapplication/controllers/JokesController.java
@@ -1,11 +1,24 @@
 package guru.springframework.jokapplication.controllers;
 
+import guru.springframework.jokapplication.services.JokesService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class JokesController {
 
+    private JokesService jokesService;
 
+    public JokesController(JokesService jokesService){
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping("/")
     public String getTheJoke(Model model){
+
+        model.addAttribute("joke",this.jokesService.getRandomQuotes());
+
         return "index";
     }
 }
